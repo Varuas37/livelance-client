d7338e64a98adfa584826d4569ff6f1bdbb0e58e
Varuas37 <saurav.panthee@gmail.com>
2022-02-02 18:27:03 -0600

    added few things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { Fragment, useEffect } from "react";
+import { Router, Switch, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import history from "./presentation/utils/history";
 import LandingPage from "./presentation/pages/Landing/LandingPage"
+import { Provider } from "react-redux";
+import { store, persistor } from "./application/redux/store/store";
+import { loadUser } from "./application/redux/action/auth";
+import setAuthToken from "./presentation/utils/setAuthToken";
 
-function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <PersistGate persistor={persistor}>
+          <Fragment>
+            <div className="App">
+              <LandingPage />
+
+            </div>
+          </Fragment>
+        </PersistGate>
+      </Router>
+    </Provider>
+  );
+}
 export default App;
--- src/application/redux/action/auth.js
+++ src/application/redux/action/auth.js
@@ -1,6 +1,9 @@
 //ANCHOR Logout
-import { LOGOUT } from "./types";
+import { LOGOUT, USER_LOADED } from "./types";
 
+export const loadUser = () => dispatch => {
+    dispatch({ type: USER_LOADED })
+}
 export const logout = () => dispatch => {
     dispatch({ type: LOGOUT })
 }
