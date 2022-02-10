a0cccff1ded1d6cfa8cb3659a090bab9e65f52fb
cvam77 <schaudhary@patriots.uttyler.edu>
2022-02-09 18:14:39 -0600

    browser router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React, { Fragment, useEffect } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import history from "./presentation/utils/history";
-import LandingPage from "./presentation/pages/Landing/LandingPage"
+import LandingPage from "./presentation/pages/Landing/LandingPage";
 import { Provider } from "react-redux";
 import { store, persistor } from "./application/redux/store/store";
 import { loadUser } from "./application/redux/action/auth";
 import setAuthToken from "./presentation/utils/setAuthToken";
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token);
 }
 
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 
-  return (
-    <Provider store={store}>
-      <Router history={history}>
-        <PersistGate persistor={persistor}>
-          <Fragment>
-            <div className="App">
-              <LandingPage />
-
-            </div>
-          </Fragment>
-        </PersistGate>
-      </Router>
-    </Provider>
-  );
-}
+	return (
+		<Provider store={store}>
+			<Router history={history}>
+				<PersistGate persistor={persistor}>
+					<Fragment>
+						<div className="App">
+							<LandingPage />
+						</div>
+					</Fragment>
+				</PersistGate>
+			</Router>
+		</Provider>
+	);
+};
 export default App;
