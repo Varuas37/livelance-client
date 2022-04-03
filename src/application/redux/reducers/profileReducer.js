0cb9149b60cbd10d5d84b18f5030dbd7837ffd85
cvam77 <schaudhary@patriots.uttyler.edu>
2022-04-03 12:50:38 -0500

    viewing own reviews

--- src/application/redux/reducers/profileReducer.js
+++ src/application/redux/reducers/profileReducer.js
@@ -4,6 +4,8 @@ import {
 	SET_REVIEWS,
 	SET_REVIEWS_DATA,
 	SET_VIEW_PROFILE,
+	SET_OWN_PROFILE_REVIEWS,
+	SET_OWN_PROFILE_REVIEWS_DATA,
 } from "../action/types";
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -17,6 +19,10 @@ export default function (state = {}, action) {
 			return { ...state, viewProfile: payload };
 		case EDIT_AND_SAVE_PROFILE:
 			return { ...state, profile: payload };
+		case SET_OWN_PROFILE_REVIEWS:
+			return { ...state, ownReviewsList: payload };
+		case SET_OWN_PROFILE_REVIEWS_DATA:
+			return { ...state, ownReviewsData: payload };
 		case SET_REVIEWS:
 			return { ...state, reviewsList: payload };
 		case SET_REVIEWS_DATA:
