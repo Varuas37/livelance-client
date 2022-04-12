0ae49b6dba0af3b89e278bf2fa5f7c79b3f758a3
cvam77 <schaudhary@patriots.uttyler.edu>
2022-04-12 17:50:26 -0500

    fixing onboarding issue

--- src/presentation/pages/Landing/LandingPage.js
+++ src/presentation/pages/Landing/LandingPage.js
@@ -277,7 +277,7 @@ function LandingPage() {
                                 to="/signup"
                                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
                             >
-                                Sign up
+                                Shivam sign up
                             </Link>
                         </div>
                     </div>
