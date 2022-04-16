6c0c45bea9d96c8675eae13e2568184306a12663
Varuas37 <saurav.panthee@gmail.com>
2022-04-16 15:44:00 -0500

    removed margin for logo

--- src/presentation/components/StaticSidebar.js
+++ src/presentation/components/StaticSidebar.js
@@ -74,7 +74,7 @@ const StaticSidebar = () => {
 		<div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
 			{/* Sidebar component, swap this element with another sidebar if you like */}
 			<div className="border-r border-gray-200 pt-5 flex flex-col flex-grow bg-white overflow-y-auto">
-				<div className="mt-5 flex-shrink-0 px-4 flex items-center">
+				<div className="flex-shrink-0 px-4 flex items-center">
 					<img
 						className="h-8 w-auto"
 						src="https://tailwindui.com/img/logos/workflow-mark-purple-600-to-indigo-600.svg"
