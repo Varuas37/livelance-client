40bb89fd6d08ec3819458917cf1056ca542fe14a
Varuas37 <saurav.panthee@gmail.com>
2022-04-16 15:47:17 -0500

    added cursor pointer to edit profile

--- src/presentation/pages/profile/UserProfile.js
+++ src/presentation/pages/profile/UserProfile.js
@@ -148,7 +148,7 @@ function UserProfile() {
 													</button> */}
 													<div
 														onClick={openEditProfileModal}
-														className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+														className="inline-flex cursor-pointer justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
 													>
 														<PencilIcon
 															className="-ml-1 mr-2 h-5 w-5 text-gray-400"
